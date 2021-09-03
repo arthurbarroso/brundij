8ad3006a1971745ffdea1c0c94b327e6f7caba96
arthur <arthurdsb_@hotmail.com>
2021-09-03 14:20:54 +0000

    feat(app): add CI workflow
    Adds a bare minimum setup for the CI. Only runs the server's tests as of now.

--- dev-resources/circleci_test/config.clj
+++ dev-resources/circleci_test/config.clj
@@ -1,31 +1,6 @@
-(require '[integrant.core :as ig]
-         '[integrant.repl :as ig-repl]
-         '[environ.core :refer [env]]
-         '[datahike.api :as d])
-
-(def config-map
-  {:server/jetty {:handler (ig/ref :brundij/app)
-                  :port (Integer/parseInt (env :port))}
-   :brundij/app {:database (ig/ref :db/postgres)
-                 :auth (ig/ref :auth/env)}
-   :db/postgres {:host (env :database-host)
-                 :port (env :database-port)
-                 :user (env :database-user)
-                 :password (env :database-password)
-                 :dbname (env :database-name)}
-   :auth/env {:jwt-secret (env :jwt-secret)}})
+(require '[integrant.repl :as ig-repl])
 
 (def reset-all ig-repl/reset-all)
-
-(defn clear-database! []
-  (let [config {:store {:backend :pg
-                        :host (get-in config-map [:db/postgres :host])
-                        :port (get-in config-map [:db/postgres :port])
-                        :username (get-in config-map [:db/postgres :user])
-                        :password (get-in config-map [:db/postgres :password])
-                        :path (str "/" (get-in config-map [:db/postgres :dbname]))}}]
-    (d/delete-database config)
-    (d/create-database config)))
 
 {:global-fixture (fn [f]
                    (reset-all)
