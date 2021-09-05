2348f3e3f6206adef401b7b635b102f261c8ae0b
arthur <arthurdsb_@hotmail.com>
2021-09-05 20:11:09 +0000

    feat(ci): add karma client tests
    * feat(app): add karma setup for client tests
    
    * chore(ci): run karma tests
    
    * chore(ci): fix yaml lol
    
    * chore(ci): rm extra lein step
    
    * fix(ci): rm typo

--- test/cljs/brundij/first_test.cljs
+++ test/cljs/brundij/first_test.cljs
@@ -1,14 +1,5 @@
 (ns cljs.brundij.first-test
-  (:require [brundij.components.title :refer [title]]
-            [cljs.test :refer-macros [deftest is testing use-fixtures]]
-            [dommy.core :as d]
-            [reagent.dom :as rdom]))
-
-(defn dom-cleanup [f]
-  (d/clear! (.getElementById js/document "app"))
-  (f))
-
-(use-fixtures :each dom-cleanup)
+  (:require [cljs.test :refer-macros [deftest is testing]]))
 
 #_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
 (deftest first-test
@@ -19,8 +10,3 @@
 (deftest second-test
   (testing "chiclete"
     (is (= 2 2))))
-
-#_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
-(deftest title-component-test
-  (rdom/render [title] (.getElementById js/document "app"))
-  (is (= (d/html (d/sel1 :h1)) "Hey!")))
