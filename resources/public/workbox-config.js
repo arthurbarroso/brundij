b116418cd73250cc45631ba044ecc7896965c36f
arthurbarroso <arthurdsb_@hotmail.com>
2021-09-16 18:11:39 -0300

    chore(app): try workbox

---
+++ resources/public/workbox-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  "globDirectory": "resources/public",
+  "globPatterns": [
+    "**/*.{html,js,edn,json}"
+  ],
+  "swDest": "resources/public/sw.js",
+  "swSrc": "src/sw.js"
+};
