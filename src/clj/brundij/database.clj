b48c269b77c51dba80dc02cb146774ece5857d53
arthurbarroso <arthurdsb_@hotmail.com>
2021-09-16 20:13:06 -0300

    feat(app): share database schema in cljc

--- src/clj/brundij/database.clj
+++ src/clj/brundij/database.clj
@@ -1,5 +1,6 @@
 (ns brundij.database
-  (:require [datahike-postgres.core]
+  (:require [brundij.schema :refer [schema]]
+            [datahike-postgres.core]
             [datahike.api :as d]))
 
 (defn create-config [{:keys [host port user password dbname]}]
@@ -9,61 +10,6 @@
            :username user
            :password password
            :path (str "/" dbname)}})
-
-(def schema [;;health-check
-             ;;health/uuid
-             {:db/ident :health/uuid
-              :db/valueType :db.type/uuid
-              :db/unique :db.unique/identity
-              :db/cardinality :db.cardinality/one}
-             ;;health/created_at
-             {:db/ident :health/created_at
-              :db/valueType :db.type/instant
-              :db/cardinality :db.cardinality/one}
-             ;;health/question
-             {:db/ident :health/question
-              :db/valueType :db.type/ref
-              :db/cardinality :db.cardinality/many}
-             ;;questions
-             ;;question/uuid
-             {:db/ident :question/uuid
-              :db/valueType :db.type/uuid
-              :db/unique :db.unique/identity
-              :db/cardinality :db.cardinality/one}
-             ;;question/created_at
-             {:db/ident :question/created_at
-              :db/valueType :db.type/instant
-              :db/cardinality :db.cardinality/one}
-             ;;question/content
-             {:db/ident :question/content
-              :db/valueType :db.type/string
-              :db/cardinality :db.cardinality/one}
-             ;;question/answer
-             {:db/ident :question/answer
-              :db/valueType :db.type/ref
-              :db/cardinality :db.cardinality/many}
-             ;;answers
-             ;;answer/uuid
-             {:db/ident :answer/uuid
-              :db/valueType :db.type/uuid
-              :db/unique :db.unique/identity
-              :db/cardinality :db.cardinality/one}
-             ;;answer/rating
-             {:db/ident :answer/rating
-              :db/valueType :db.type/bigint
-              :db/cardinality :db.cardinality/one}
-             ;;answer/rating
-             {:db/ident :answer/rating
-              :db/valueType :db.type/bigint
-              :db/cardinality :db.cardinality/one}
-             ;;answer/trend
-             {:db/ident :answer/trend
-              :db/valueType :db.type/string
-              :db/cardinality :db.cardinality/one}
-             ;;answer/created_at
-             {:db/ident :answer/created_at
-              :db/valueType :db.type/instant
-              :db/cardinality :db.cardinality/one}])
 
 (defn create-database [config]
   (d/create-database config))
