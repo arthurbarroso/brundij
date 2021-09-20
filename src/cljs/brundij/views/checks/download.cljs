c582ef4787275668d17577c3c0e2c5b0bcd56108
arthurbarroso <arthurdsb_@hotmail.com>
2021-09-19 21:23:10 -0300

    fix(client): fix download input/button

--- src/cljs/brundij/views/checks/download.cljs
+++ src/cljs/brundij/views/checks/download.cljs
@@ -10,18 +10,20 @@
 (defn download-check-results-view []
   (let [health-id-input (re-frame/subscribe [::subs/health-id-input])]
     [template
-     [:h3 (use-style {:font-size "3rem" :margin 0
-                      :max-width "80%"}) "Download health check results 🧶"]
-     [:p (use-style {:max-width "75%"})
-      "Type in your health check's id and press the button to download it's results"]
-     [input {:value @health-id-input
-             :on-change #(re-frame/dispatch [::cevts/change-health-id-input %])
-             :type "text"
-             :placeholder "Your health check's id"
-             :extra-styles {:max-width "75%"}}]
-     [button {:on-click #(re-frame/dispatch [::cevts/fetch-results @health-id-input])
-              :text "Download health check's results"
-              :disabled false
-              :extra-styles {:margin-top "1%"
-                             :max-width "75%"
-                             :color "#333"}}]]))
+     [:div (use-style {:margin-top "10%"})
+      [:h3 (use-style {:font-size "3rem" :margin 0
+                       :max-width "80%"}) "Download health check results 🧶"]
+      [:p (use-style {:max-width "75%"})
+       "Type in your health check's id and press the button to download it's results"]
+      [input {:value @health-id-input
+              :on-change #(re-frame/dispatch [::cevts/change-health-id-input %])
+              :type "text"
+              :placeholder "Your health check's id"
+              :extra-styles {:width "100%"}}]
+      [button {:on-click #(re-frame/dispatch [::cevts/fetch-results @health-id-input])
+               :text "Download health check's results"
+               :disabled false
+               :title "Download results"
+               :extra-styles {:margin-top "1%"
+                              :color "#333"
+                              :width "100%"}}]]]))
