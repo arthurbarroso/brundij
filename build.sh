b164838c5e6ffe698c72793d30b50c95e22d32c1
arthurbarroso <arthurdsb_@hotmail.com>
2021-09-26 19:27:06 -0300

    feat(pre-rendering): pre-render build script

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+npm run release
+cp ./resources/public/js/app.js ./pre-render/resources/assets/app.js
+cp ./resources/public/js/manifest.edn ./pre-render/resources/assets/manifest.edn
+npm run serve-pre-render &
+lein pre-render
+killall -9 node
+
+LEIN_SNAPSHOTS_IN_RELEASE=true lein uberjar
