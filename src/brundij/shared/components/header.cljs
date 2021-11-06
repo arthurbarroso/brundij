f7e889d7922f5253e0b35df1dc5d5c4abf4fb6f6
arthurbarroso <arthurdsb_@hotmail.com>
2021-11-06 19:04:53 -0300

    feat(client): add results link to header

--- src/brundij/shared/components/header.cljs
+++ src/brundij/shared/components/header.cljs
@@ -1,4 +1,9 @@
-(ns brundij.shared.components.header)
+(ns brundij.shared.components.header
+  (:require [re-frame.core :as re-frame]
+            [brundij.shared.events :as events]))
+
+(defn navigate-to-brundi-gh []
+  (.open js/window "https://github.com/arthurbarroso/brundij"))
 
 (defn header []
   [:div {:class "header-style"}
@@ -6,5 +11,9 @@
         :href "/"}
     "brundij"]
    [:nav {:class "header-container-style"}
-    [:a {:class "link-style"}
-     "About"]]])
+    [:button {:class "link-style"
+              :on-click #(navigate-to-brundi-gh)}
+     "About"]
+    [:button {:class "link-style"
+              :on-click #(re-frame/dispatch [::events/navigate "/results"])}
+     "Results"]]])
