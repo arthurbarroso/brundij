518b5ea3efc57ca42d226d3d437ce011dc2e1c90
arthurbarroso <arthurdsb_@hotmail.com>
2021-11-06 17:42:59 -0300

    chore(client): fix client tests

--- test/cljs/brundij/button_test.cljs
+++ test/cljs/brundij/button_test.cljs
@@ -1,24 +1,15 @@
 (ns brundij.button-test
-  (:require [brundij.components.button :refer [button]]
+  (:require [brundij.shared.components.button :refer [button]]
             [cljs.test :refer-macros [deftest is testing use-fixtures]]
             [dommy.core :as d]
             [reagent.core :as r]
             [reagent.dom :as rdom]
-            [stylefy.core :as stylefy]
-            [stylefy.reagent :as stylefy-reagent]
             ["react-dom/test-utils" :as dom-test-utils]))
 
 (defn create-app-element [f]
   (.appendChild (.-body js/document)
                 (doto (.createElement js/document "div")
                   (-> (.setAttribute "id" "app"))))
-  (.appendChild (.-head js/document)
-                (doto (.createElement js/document "style")
-                  (-> (.setAttribute "id" "_stylefy-constant-styles_"))))
-  (.appendChild (.-head js/document)
-                (doto (.createElement js/document "style")
-                  (-> (.setAttribute "id" "_stylefy-styles_"))))
-  (stylefy/init {:dom (stylefy-reagent/init)})
   (f))
 
 (defn dom-cleanup [f]
@@ -40,25 +31,10 @@
   (testing "Renders correctly"
     (rdom/render [button {:on-click #(println "hi")
                           :text "Click me"
-                          :disabled false
-                          :extra-styles {}}]
+                          :disabled false}]
                  (appended-container (.getElementById js/document "app")
                                      "button"))
-    (is (= (d/html (d/sel1 :button)) "Click me"))
-    (is (= (d/style (d/sel1 :button) :cursor) "pointer"))
-    (is (= (d/style (d/sel1 :button) :font-weight) "500"))))
-
-#_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
-(deftest button-component-extra-styles-test
-  (testing "Changes styles based on extra-styles"
-    (rdom/render [button {:on-click #(println "hi")
-                          :text "Click me"
-                          :disabled false
-                          :extra-styles {:font-weight 700}}]
-                 (appended-container (.getElementById js/document "app")
-                                     "button2"))
-    (is (= (d/html (d/sel1 :button)) "Click me"))
-    (is (= (d/style (d/sel1 :button) :font-weight) "700"))))
+    (is (= (d/html (d/sel1 :button)) "Click me"))))
 
 #_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
 (deftest button-component-click-test
@@ -66,8 +42,7 @@
     (let [ra (r/atom 1)]
       (rdom/render [button {:on-click #(swap! ra inc)
                             :text "Click me"
-                            :disabled false
-                            :extra-styles {:font-weight 700}}]
+                            :disabled false}]
                    (appended-container (.getElementById js/document "app")
                                        "button2"))
       (.click dom-test-utils/Simulate (d/sel1 :button))
