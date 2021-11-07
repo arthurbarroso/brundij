09ab4e26ecda657fafa09e7eedfd80421e501fde
arthurbarroso <arthurdsb_@hotmail.com>
2021-11-07 00:45:35 -0300

    fix(kaocha): add fixture

--- src/util/test_utils.clj
+++ src/util/test_utils.clj
@@ -1,6 +1,28 @@
 (ns util.test-utils
   (:require [integrant.repl :as ig-repl]
-            [taoensso.timbre :as timbre]))
+            [taoensso.timbre :as timbre]
+            [brundij.server]
+            [integrant.core :as ig]
+            [clojure.java.io :as io]
+            [aero.core :refer [read-config]]))
+
+(def environment-vars
+  (read-config (io/resource "config.edn") {:profile :test}))
+
+(def config-map
+  {:server/jetty {:handler (ig/ref :brundij/app)
+                  :port (:port environment-vars)}
+   :brundij/app {:database (ig/ref :db/postgres)}
+   :db/postgres {:host (:database_host environment-vars)
+                 :port (:database_port environment-vars)
+                 :user (:database_user environment-vars)
+                 :backend (:database_backend environment-vars)
+                 :id (:database_id environment-vars)
+                 :password (:database_password environment-vars)
+                 :dbname (:database_name environment-vars)}})
+
+(ig-repl/set-prep!
+ (fn [] config-map))
 
 (def reset-all ig-repl/reset-all)
 
